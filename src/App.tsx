57adfd5080ce9b4ab871c1eeff00b46433b025c0
Dave <davstar47@gmail.com>
2025-12-20 11:30:13 -0500

    Fix Navbar filename casing for Linux build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import Write from "./pages/Write";
 import Post from "./pages/Post";
